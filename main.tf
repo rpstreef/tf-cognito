3decd624fcf5be0f9eb108ac075b76cee0fca506
Rolf Streefkerk <r.streefkerk@gmail.com>
2021-04-22 12:02:34 +0700

    fix specific record

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "aws_route53_record" "auth-cognito-A" {
 
   name    = aws_cognito_user_pool_domain._.domain
   type    = "A"
-  zone_id = data.aws_route53_zone._.zone_id
+  zone_id = data.aws_route53_zone._.*.zone_id
 
   alias {
     evaluate_target_health = false
