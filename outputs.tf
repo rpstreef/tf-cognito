eca0e2691dec2d6a0124da4efc40785189267853
Rolf Streefkerk <r.streefkerk@gmail.com>
2021-04-22 11:46:58 +0700

    added user pool outputs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,11 @@ output "cognito_user_pool_client_id" {
 output "cognito_identity_pool_id" {
   value = aws_cognito_identity_pool._.id
 }
+
+output "cognito_user_pool_domain" {
+  value = aws_cognito_user_pool_domain._.domain
+}
+
+output "cognito_user_pool_cloudfront_arn" {
+  value = aws_cognito_user_pool_domain._.cloudfront_distribution_arn
+}
